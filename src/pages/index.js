3b6e5eb1836ed6449b3458972bda2d5418329387
Richard Germaine <rlgerma92@outlook.com>
2020-04-15 23:52:51 -0600

    typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import SignupForm from '../components/Signup';
+import SignupForm from '../components/SignUp';
 import SlideShow from '../components/SlideShow';
 
 var settings = {
